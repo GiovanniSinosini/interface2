fb2a519fa2fe5464dda2654abf288406d48113d4
Giovanni Sinosini <carvalho.sino@gmail.com>
2020-01-26 03:05:03 +0000

    Solution with interface

--- src/model/entities/Shape.java
+++ src/model/entities/Shape.java
@@ -1,22 +1,7 @@
 package model.entities;
 
-import model.enums.Color;
 
-public abstract class Shape {
+public interface Shape {
 
-	private Color color;
-
-	public Shape(Color color) {
-		this.color = color;
-	}
-
-	public Color getColor() {
-		return color;
-	}
-
-	public void setColor(Color color) {
-		this.color = color;
-	}
-
-	public abstract double area();
+	double area();
 }
